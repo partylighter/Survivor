8a0117267ecbf633f48cbcbe9e205085e56efc92
JordanNiambault <niambaultmarketing@gmail.com>
2025-11-22 07:07:27 +0100

    Modif sur certains params

--- scripts/Player/player.gd
+++ scripts/Player/player.gd
@@ -3,6 +3,9 @@ class_name Player
 
 @export var speed: float = 500.0
 
+func _ready() -> void:
+	add_to_group("joueur_principal")
+
 func _physics_process(_dt: float) -> void:
 	var dir := Input.get_vector("gauche","droite","haut","bas")
 	velocity = dir.normalized() * speed
